7c1dc4de88df3f9547f7104ce9cff44edf015fc6
Asadullah850 <mhshamim850@gmail.com>
2023-02-14 11:18:00 +0600

    allCheck

--- jsAllFile/js.js
+++ jsAllFile/js.js
@@ -1,21 +1,22 @@
-// document.getElementById('email').addEventListener('focus',function(){
-//     let fromBody =  document.getElementById('fromBody');
-//     let p = document.createElement('p').innerHTML = 'Type- test@email.com';
-//     // console.log(p);
-//     p.appendChild(fromBody);
-// })
+document.getElementById('email').addEventListener('mouseup',function(){
+    // let fromBody =  document.getElementById('fromBody');
+    // let p = document.createElement('p').innerHTML = 'Type- test@email.com';
+    // console.log(p);
+    // p.appendChild(fromBody);
+    alert('Type- test@email.com && pass=test')
+})
+
 document.getElementById('Login-butt').addEventListener('click',function(){
     let emailBox = document.getElementById('email');
     let emailValue = emailBox.value;
     // console.log(emailValue);
     let passwordBox = document.getElementById('password').value; 
     
-    if (emailValue === 'in' && passwordBox == 'in') {
-        window.location.href='inside.html';
+    if (emailValue === 'test@email.com' && passwordBox == 'test') {
+        window.location.href='./inside.html';
     }
     else{
         console.log('Input Correct Information');
     }
     
 })
-// 
